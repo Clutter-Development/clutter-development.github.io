dd0fdba3dc333d1ae5e5863434206d0a91734be3
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-04-24 10:07:16 +0300

    dark mode toggler

---
+++ javascript/index.js
@@ -0,0 +1,25 @@
+const toggleSwitch = document.getElementById('checkbox');
+const currentTheme = localStorage.getItem('theme');
+
+console.log(document);
+console.log(toggleSwitch);
+console.log(currentTheme);
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'light') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'dark');
+          localStorage.setItem('theme', 'dark');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
